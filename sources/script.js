b98fc122813bb60171dc64e54886d4e64cb6cdb9
EvgeniyBuyanov <evgeniy.buyanov@carprice.ru>
2021-10-06 16:13:01 +0300

    dat-v-eblo

--- sources/script.js
+++ sources/script.js
@@ -2,13 +2,33 @@ window.addEventListener('load', () => {
     const   sanekImg = document.querySelector('.sanek'),
             fingerBtn = document.querySelector('.finger'),
             gayBtn = document.querySelector('.gay'),
+            ebloBtn = document.querySelector('.eblo'),
             pisunBtn = document.querySelector('.pisun');
 
     sanekImg.addEventListener('click', () => play('tupa_kal', true));
     fingerBtn.addEventListener('click', () => ajax('finger', renderText, () => play('ne_navizu_raz')))
     gayBtn.addEventListener('click', () => ajax('gay', renderText, () => play('tobi_pizda')))
+    ebloBtn.addEventListener('click', () => uebat())
     pisunBtn.addEventListener('click', () => ajax('hui', renderText, () => play('but_v_otgule')))
 });
+
+function uebat() {
+    const hand = document.querySelector('.hand');
+    const fofan = document.querySelector('.fofan');
+    hand.style.display = 'block';
+    const start = Date.now();
+    const timer = setInterval(() => {
+        const timePassed = Date.now() - start;
+        if (timePassed >= 2000) {
+            clearInterval(timer);
+            hand.style.display = 'none';
+            fofan.style.display = 'block';
+            setTimeout(() => fofan.style.display = 'none', 3000)
+            return;
+        }
+        hand.style.right = timePassed / 3 + 'px';
+    }, 20);
+}
 
 function renderText(text) {
     const input = document.querySelector('.say');
