4f9bbdfa5e03d3204159d564d4f823bde4dde243
EvgeniyBuyanov <evgeniy.buyanov@carprice.ru>
2021-10-11 19:09:46 +0300

    game

--- sources/script.js
+++ sources/script.js
@@ -1,18 +1,71 @@
 window.addEventListener('load', () => {
-    const   sanekImg = document.querySelector('.sanek'),
-            fingerBtn = document.querySelector('.finger'),
-            gayBtn = document.querySelector('.gay'),
-            ebloBtn = document.querySelector('.eblo'),
-            buhBtn = document.querySelector('.buhat'),
-            pisunBtn = document.querySelector('.pisun');
+    document.onkeydown = moveSanek;
+
+    const sanekImg = document.querySelector('.sanek'),
+        fingerBtn = document.querySelector('.finger'),
+        gayBtn = document.querySelector('.gay'),
+        ebloBtn = document.querySelector('.eblo'),
+        buhBtn = document.querySelector('.buhat'),
+        gameBtn = document.querySelector('.game'),
+        pisunBtn = document.querySelector('.pisun');
 
     sanekImg.addEventListener('click', () => play('tupa_kal', true));
     fingerBtn.addEventListener('click', () => ajax('finger', renderText, () => play('ne_navizu_raz')))
     gayBtn.addEventListener('click', () => ajax('gay', renderText, () => play('tobi_pizda')))
     ebloBtn.addEventListener('click', () => uebat())
     buhBtn.addEventListener('click', () => nabuhat())
+    gameBtn.addEventListener('click', () => game())
     pisunBtn.addEventListener('click', () => ajax('hui', renderText, () => play('but_v_otgule')))
 });
+
+function moveSanek() {
+    const hui = document.querySelector('.hui');
+    if (hui.dataset.game !== 'yee') {
+        return;
+    }
+    hui.style.bottom = '85%';
+    setTimeout(() => hui.style.bottom = '0', 500);
+}
+
+function game() {
+    const sanek = document.querySelector('.sanek'),
+        buhaet = document.querySelector('.buhaet'),
+        say = document.querySelector('.say'),
+        talk = document.querySelector('.talk');
+
+    sanek.hidden = true;
+    talk.hidden = true;
+    say.hidden = true;
+    buhaet.hidden = false;
+    buhaet.style.height = '80%';
+    setTimeout(() => moveHui(), 1000);
+    play('mario', true, 'mp3', false)
+}
+
+function moveHui() {
+    const hui = document.querySelector('.hui');
+    const sperma = document.querySelector('.sperma');
+    hui.dataset.game = 'yee';
+    hui.hidden = false;
+    let start = Date.now();
+    const interval = setInterval(() => {
+        const timePassed = Date.now() - start;
+        let move = timePassed / 20;
+        if (move >= 90) {
+            start = Date.now();
+        }
+        hui.style.right = move  + '%';
+        const styles = getComputedStyle(hui);
+        if (move === 50 && styles.bottom === '0px') {
+            sperma.hidden = false;
+            clearInterval(interval);
+            setTimeout(() => {
+                alert('game over');
+                window.location.reload();
+            }, 100);
+        }
+    }, 20);
+}
 
 function nabuhat() {
     const sanek = document.querySelector('.sanek');
@@ -51,7 +104,8 @@ function renderText(text) {
     input.innerHTML = text;
 }
 
-function play(file, loop = false, ext = 'ogg', showRot = true, callback = () => {}) {
+function play(file, loop = false, ext = 'ogg', showRot = true, callback = () => {
+}) {
     const rot = document.querySelector('.rot');
     const audio = new Audio(`/sources/audio/${file}.${ext}`);
     audio.loop = loop;
